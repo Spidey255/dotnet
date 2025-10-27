4d9232c09c69e75764fff36e3a8efa268a7b33aa
Srini255 <srinitamil17@gmail.com>
2025-10-27 13:13:03 +0530

    new test deployement c#"

--- TestProject/obj/Debug/net6.0/TestProject.AssemblyInfo.cs
+++ TestProject/obj/Debug/net6.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+994136b6da6f8c96d95407d97449f8cbc8ab7496")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
